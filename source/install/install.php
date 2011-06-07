8bc3d2c5b7010874427fdf5d2bd199c73e659607
khushboo <khushboo@67289b8e-1f33-4930-9c80-eb9a56b8fffd>
2011-06-07 12:38:44 +0000

    modified install.php and install.sql

--- source/install/install.php
+++ source/install/install.php
@@ -23,7 +23,10 @@ function com_install()
 function setup_database()
 {		
 	if(isTableExist('jspc_addons'))
+	{
+		insertSampleData();
 		return true;
+	}
 
 	return false;
 }
@@ -87,4 +90,31 @@ function installExtensions($extPath=null)
 	}
 
 	return true;
+}
+
+function insertSampleData()
+{       
+        $query          = " SELECT count(`id`) FROM `#__jspc_addons` ";
+        
+        $db		= JFactory::getDBO();
+        $db->setQuery( $query );
+        $result		=$db->query();
+        if($result)
+                return;
+                
+        $insertQuery = "INSERT INTO `#__jspc_addons` (`id`, `name`, `featurename`, `coreparams`, `addonparams`, `published`)"
+                                        ." VALUES (1, 'albums', 'Create 8 Album', 'jspc_core_total_contribution=100\njspc_core_display_text=Create %s Album\njspc_profiletype=0\n\n', 'albums_total=8\n\n', 1),"
+                                        ." (2, 'groupowner', 'Create 5 Group', 'jspc_core_total_contribution=90\njspc_core_display_text=Create %s Group\njspc_profiletype=0\n\n', 'groupowner_total=5\n\n', 1),"
+                                        ." (3, 'photos', 'Add 9 Photos', 'jspc_core_total_contribution=60\njspc_core_display_text=Add %s Photos\njspc_profiletype=0\n\n', 'photos_total=9\n\n', 1),"
+                                        ." (4, 'avatar', 'Upload Avtar', 'jspc_core_total_contribution=40\njspc_core_display_text=Upload Avtar\njspc_profiletype=0\n\n', '\n', 1),"
+                                        ." (5, 'groupmember', 'Join 6 Group', 'jspc_core_total_contribution=70\njspc_core_display_text=Join %s Group\njspc_profiletype=0\n\n', 'groupmember_total=6\n\n', 1),"
+                                        ." (6, 'videos', 'Upload 4 Videos', 'jspc_core_total_contribution=40\njspc_core_display_text=Upload %s Videos\njspc_profiletype=0\n\n', 'videos_total=4\n\n', 1),"
+                                        ." (7, 'profilefields', 'Edit Profile', 'jspc_core_total_contribution=80\njspc_core_display_text=Edit Profile\njspc_profiletype=0\n\n', '2=8\n3=4\n4=4\n5=8\n7=8\n8=4\n9=8\n10=8\n11=4\n12=8\n13=4\n15=4\n16=8\n\n', 1)";
+        
+        $db= JFactory::getDBO();
+        $db->setQuery( $insertQuery );
+        $db->query();
+        
+        return;
+
 }
